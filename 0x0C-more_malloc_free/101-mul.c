4b686c7aa2678d645227e73e748f793702b583c5
Meekaylah <kaylahusanga@gmail.com>
2022-06-30 09:00:42 -0700

    Create files for 0x0C-more_malloc_free directory

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,7 +8,6 @@
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-
 int is_digit(char *s)
 {
 	int i = 0;
@@ -28,7 +27,6 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
 	int i = 0;
@@ -43,7 +41,6 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
-
 void errors(void)
 {
 	printf("Error\n");
@@ -57,21 +54,18 @@ void errors(void)
  *
  * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
